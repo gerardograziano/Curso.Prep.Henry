e36e52a90eb2a6625681569e5775ea4a76f3bf38
gerardograziano <cggraziano@gmail.com>
2021-11-27 00:32:06 -0300

    correccion 02-JS-1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -109,20 +109,20 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  return (num**2);
+  return Math.pow(num,2);
   
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  return (num**3);
+  return Math.pow(num,3);
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-  return (num**exponent);
+  return Math.pow(num,exponent);
 }
 
 function redondearNumero(num) {
